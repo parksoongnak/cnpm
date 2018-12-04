b374aea38577f383fb0cd0ac5023e3437f33fb6f
parsoongnak <tranminhnhat1005@gmail.com>
2018-12-04 18:17:52 +0700

    DoAnCNPM da chinh sua

--- DoAnCNPM/Form1.Designer.cs
+++ DoAnCNPM/Form1.Designer.cs
@@ -34,6 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Fuchsia;
             this.ClientSize = new System.Drawing.Size(585, 485);
             this.Name = "Form1";
             this.Text = "Form1";
